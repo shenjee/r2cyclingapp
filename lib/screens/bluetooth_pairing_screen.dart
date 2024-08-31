111a8df3dac8471f88e6ca9db09c57a0f6279068
jishen <9263160+r2next@user.noreply.gitee.com>
2024-08-31 14:13:10 +0800

    refactory the ui of bluetooth pairing screen, and added a connecting animation

--- lib/screens/bluetooth_pairing_screen.dart
+++ lib/screens/bluetooth_pairing_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 // ble
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
@@ -15,22 +16,25 @@ class BluetoothPairingScreen extends StatefulWidget {
   State<BluetoothPairingScreen> createState() => _BluetoothPairingScreenState();
 }
 
-class _BluetoothPairingScreenState extends State<BluetoothPairingScreen> {
+class _BluetoothPairingScreenState extends State<BluetoothPairingScreen> with TickerProviderStateMixin {
   final R2BluetoothModel _bluetoothModel = R2BluetoothModel();
   bool _isScanning = false;  // title
   String _title = '开始连接您的智能头盔';
   // ble
   DiscoveredDevice? connectedDevice;// classic bluetooth
 
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
   @override
   void initState() {
     super.initState();
     requestPermissions();
-    //_addListener();
   }
 
   @override
   void dispose() {
+    _animationController.dispose();
     _bluetoothModel.stopScan();
     super.dispose();
   }
@@ -94,7 +98,7 @@ class _BluetoothPairingScreenState extends State<BluetoothPairingScreen> {
   void _startScanning() {
     setState(() {
       _isScanning = true;
-      _title = '正在连接您的智能头盔';
+      _title = '请选择您的智能头盔';
     });
 
     _bluetoothModel.scanDevices();
@@ -103,19 +107,35 @@ class _BluetoothPairingScreenState extends State<BluetoothPairingScreen> {
   Future<void> _onDeviceSelected(DiscoveredDevice device) async {
     // stop bluetooth scanning
     _bluetoothModel.stopScan();
-
+    _isScanning = false;
+    _title = '正在连接您的智能头盔';
     setState(() {
       connectedDevice = device;
     });
-    try {
-      //await _bluetoothModel.connectDevice(device.id);
+
+    // Initialize the animation controller for twinkling effect
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+    _animationController.forward();
+
+    _animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _animationController.reset();
+        _animationController.forward();
+      }
+    });
+
+    // Execute the code after the animation completes
+    Future.delayed(const Duration(seconds: 3), () async {
       await widget.onDeviceConnected(device); // Notify the parent widget
       if (mounted) {
         Navigator.of(context).pop(true);
       }
-    } catch (e) {
-      debugPrint('Connection failed: $e');
-    }
+    });
 
     // After BLE connection, initiate Bluetooth Classic connection
   }
@@ -168,29 +188,32 @@ class _BluetoothPairingScreenState extends State<BluetoothPairingScreen> {
   Widget _scanningWidget() {
     return Column(
       children: [
-        const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.phone_android, size: 48.0),
-            SizedBox(width: 16.0),
-            Icon(Icons.bluetooth, size: 48.0),
-            SizedBox(width: 16.0),
-            Icon(Icons.cyclone, size: 48.0),
-          ],
-        ),
         const SizedBox(height: 16.0),
         StreamBuilder<List<DiscoveredDevice>>(
             stream: _bluetoothModel.scannedDevices,
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.active && snapshot.hasData) {
                 return Column(
-                  children: snapshot.data!.map((device) {
-                    return ListTile(
-                      title: Text(device.name),
-                      subtitle: Text(device.id),
-                      onTap: () => _onDeviceSelected(device),
-                    );
-                  }).toList(),
+                  children: snapshot.data!.map(
+                          (device) {
+                            return Container(
+                              margin: const EdgeInsets.symmetric(vertical: 8.0),  // Optional: Adds spacing between items
+                              decoration: BoxDecoration(
+                                color: const Color(0xFF539765),  // Set your desired background color here
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
+                              child: ListTile(
+                                textColor: Colors.white,
+                                title: Text(device.name),
+                                contentPadding: const EdgeInsets.symmetric(
+                                  vertical: 10.0,
+                                  horizontal: 16.0,
+                                ),
+                                trailing: Icon(Icons.chevron_right, color: Colors.grey[200]),
+                                onTap: () => _onDeviceSelected(device),
+                              ),
+                            );
+                          }).toList(),
                 );
               } else {
                 return const Text("正在扫描...");
@@ -201,12 +224,56 @@ class _BluetoothPairingScreenState extends State<BluetoothPairingScreen> {
     );
   }
 
+  Widget _connectingWidget() {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.smartphone, size: 44, color: Colors.grey[700]),
+          const SizedBox(width: 8.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // fuck it, stupid flutter , u have to ... use sized box.
+              const SizedBox(height:140.0),
+              Icon(Icons.bluetooth, size: 36, color: Colors.grey[700]),
+              const SizedBox(height: 8.0,),
+              AnimatedBuilder(
+                  animation: _animation,
+                  builder: (context, child) {
+                    return Row(
+                      children: [
+                        for (int i = 0; i < 10; i++) ...[
+                          Opacity(
+                            opacity: _animation.value > i*0.1? 1.0:0.0,
+                            child: Container(
+                              width: 10,
+                              height: 2,
+                              margin: const EdgeInsets.symmetric(horizontal: 2),
+                              color: Colors.grey[700],
+                            ),
+                          )
+                        ],
+                      ],
+                    );}
+              ),
+            ],
+          ),
+          const SizedBox(width: 8.0),
+          Image.asset('assets/icons/cycling_helmet.png', width: 52.0, height: 52.0),
+        ],
+      ),
+    );
+  }
+
   Widget _centerWidget() {
     Widget w;
     if (true == _isScanning) {
       w = _scanningWidget();
-    } else {
+    } else if (connectedDevice == null) {
       w = _instructionWidget();
+    } else {
+      w = _connectingWidget();
     }
 
     return w;
@@ -219,6 +286,8 @@ class _BluetoothPairingScreenState extends State<BluetoothPairingScreen> {
         leading: IconButton(
           icon: const Icon(Icons.close, size: 34.0,),
           onPressed: () {
+            // stop device scanning
+            _bluetoothModel.stopScan();
             Navigator.of(context).pop(false);
             },
         ),
@@ -234,15 +303,18 @@ class _BluetoothPairingScreenState extends State<BluetoothPairingScreen> {
               ),
             ),
             Container(
-              padding: const EdgeInsets.all(0.0),
+              padding: const EdgeInsets.all(10.0),
               color: Colors.grey[200],
               height: 400,
               child:_centerWidget(),
             ),
             const SizedBox(height: 50.0,),
-            R2FlatButton(text: '开始连接', onPressed: (){
-              _startScanning();
-            }),
+            if (false == _isScanning && null == connectedDevice)
+              R2FlatButton(
+                  text: '开始连接',
+                  onPressed: () {
+                    _startScanning();
+                  }),
           ]
       ),
     );
